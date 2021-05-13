d3d6a6137301849156ff2acfd901fed629113e33
Ramona <ramona1397@yahoo.com>
2021-05-13 11:48:46 +0300

    Initial commit

---
+++ phone_number.py
@@ -0,0 +1,50 @@
+# Creati un app ce va cere numarul de telfon
+# (numere arbice)
+# si va printa pe ecran numarul cu caractere.
+# 1 , 2, 3
+# unu , doi , trei
+####################################################
+
+# def phone_number():
+#     x = input("Introduceti numarul de telefon: ")
+#     print("{}-{}-{}".format(x[:3], x[3:6], x[6:]))
+
+
+# phone_number()
+
+#####################################################
+
+def transNum(string):
+    number = 1
+
+    numberElements={
+        "a":2,"b":2,"c":2,
+        "d":3,"e":3,"f":3,
+        "g":4,"h":4,"i":4,
+        "j":5,"k":5,"l":5,
+        "m":6,"n":6,"o":6,
+        "p":7,"q":7,"r":7,"s":7,
+        "t":8,"u":8,"v":8,
+        "w":9,"x":9,"y":9,"z":9,
+    }
+
+    for ch in string:
+        number = numberElements[ch.lower()]
+    return number
+
+def translate(phone):
+    newNum = ""
+    for ch in phone:
+        if ch.lower() in ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]:
+            newNum = newNum + str(transNum(ch))
+        else:
+            newNum = newNum + ch
+    return newNum
+
+def main():
+    phone = input("enter a phone number")
+    noLetters = translate(phone)
+    print("The number you entered: ", phone)
+    print("Translates to: ", noLetters)
+
+
